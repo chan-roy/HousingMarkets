d0880e24aecf30595ff242db0173881f2c87523c
Roy Chan <rk.chan@mail.utoronto.ca>
2022-04-26 18:41:09 -0400

    add summary statistics table

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,36 +1,28 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# Purpose: Convert datafiles from original paper and reproduce scripts in R
+# Author: Roy Chan
+# Data: 27 April 2022
+# Contact: rk.chan@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
+# - Need to download replication data from openICPSR 
+# - https://www.openicpsr.org/openicpsr/project/116160/version/V1/view?path=/openicpsr/116160/fcr:versions/V1&type=project
 
 
 #### Workspace setup ####
-# Use R Projects, not setwd().
 library(haven)
 library(tidyverse)
-# Read in the raw data. 
-raw_data <- readr::read_csv("inputs/data/raw_data.csv"
-                     )
-# Just keep some variables that may be of interest (change 
-# this depending on your interests)
-names(raw_data)
 
-reduced_data <- 
-  raw_data %>% 
-  select(first_col, 
-         second_col)
-rm(raw_data)
-         
+# use read_dta to read and convert .dta files to R dataframes
+econ_analysis <- read_dta("inputs/data/ui_econ_analysis.dta")
+sipp_analysis <- read_dta("inputs/data/ui_sipp_analysis.dta")
+nlsy_analysis <- read_dta("inputs/data/ui_nlsy_analysis.dta")
+nlsy_state_variables <- read_dta("inputs/data/ui_nlsy_state_variables.dta")
+hpi_analysis <- read_dta("inputs/data/ui_hpi_analysis.dta")
 
-#### What's next? ####
-
-
-
-         
+# write to csv
+write_csv(econ_analysis, "inputs/data/econ_analysis.csv")
+write_csv(sipp_analysis, "inputs/data/sipp_analysis.csv")
+write_csv(nlsy_analysis, "inputs/data/nlsy_analysis.csv")
+write_csv(nlsy_state_variables, "inputs/data/nlsy_state_variables.csv")
+write_csv(hpi_analysis, "inputs/data/hpi_analysis.csv")
